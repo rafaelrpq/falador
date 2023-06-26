d96bc6bb55cf0f157774e1d9a70f01ad0459b432
Rafael Quevedo <31359531+rafaelrpq@users.noreply.github.com>
2023-06-26 18:08:50 -0300

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,15 +1,32 @@
-var DYNAMIC_CACHE = 'v20230626-1740';
+var DYNAMIC_CACHE = 'v20230615-1332';
 var urlsToCache = [
   './index.html',
   './index.css',
   './snackbar.css',
   './index.js',
-  './AppManager.js',
-  './Falador.js',
+  './appManager.js',
   './manifest.json',
   './res/icons/128.png',
 
-  './res/bichos/',
+  './res/bichos/abelha.avif',
+  './res/bichos/baleia.avif',
+  './res/bichos/cachorro.avif',
+  './res/bichos/cavalo.avif',
+  './res/bichos/coelho.avif',
+  './res/bichos/elefante.avif',
+  './res/bichos/galinha.avif',
+  './res/bichos/gato.avif',
+  './res/bichos/jacaré.avif',
+  './res/bichos/leão.avif',
+  './res/bichos/macaco.avif',
+  './res/bichos/peixe.avif',
+  './res/bichos/porco.avif',
+  './res/bichos/tartaruga.avif',
+  './res/bichos/tigre.avif',
+  './res/bichos/tubarão.avif',
+  './res/bichos/urso.avif',
+  './res/bichos/vaca.avif',
+  './res/bichos/zebra.avif',
 ];
 
 self.addEventListener ('install', function (event) {
@@ -17,7 +34,7 @@ self.addEventListener ('install', function (event) {
     event.waitUntil (
         caches.open (DYNAMIC_CACHE)
         .then (function (cache) {
-            // console.log ('Opened cache');
+            console.log ('Opened cache');
             return cache.addAll (urlsToCache);
         })
     );
