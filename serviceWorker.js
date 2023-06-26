cddd8ab01613238a80bbcca694a28dc7ed3672a7
Rafael Quevedo <rafaelrpq@gmail.com>
2023-06-26 19:42:52 -0300

    teste de correção

--- serviceWorker.js
+++ serviceWorker.js
@@ -7,42 +7,19 @@ var urlsToCache = [
   './AppManager.js',
   './Falador.js',
   './manifest.json',
-  './res/icons/128.png',
+  // './res/icons/128.png',
 
   './res/bichos/',
 ];
 self.addEventListener ('install',  async function (event) {
-    // self.skipWaiting ();
-    // event.waitUntil (
-    //     caches.open (DYNAMIC_CACHE)
-    //     .then (function (cache) {
-    //         // console.log ('Opened cache');
-    //         return cache.addAll (urlsToCache);
-    //     })
-    // );
-    try {
-      // ok = await cache.addAll(urlsToCache);
-      event.waitUntil (
-          caches.open (DYNAMIC_CACHE)
-          .then (function (cache) {
-              // console.log ('Opened cache');
-              return cache.addAll (urlsToCache);
-          })
-      );
-    } catch (err) {
-      console.error('sw: cache.addAll');
-      for (let i of urlsToCache) {
-        try {
-          caches.open (DYNAMIC_CACHE)
-          .then (function (cache) {
-              // console.log ('Opened cache');
-              return cache.add(i);
-          })
-        } catch (err) {
-          console.warn('sw: cache.add',i);
-        }
-      }
-    }
+    self.skipWaiting ();
+    event.waitUntil (
+        caches.open (DYNAMIC_CACHE)
+        .then (function (cache) {
+            // console.log ('Opened cache');
+            return cache.addAll (urlsToCache);
+        })
+    );
 });
 
 self.addEventListener('message', function (event) {
